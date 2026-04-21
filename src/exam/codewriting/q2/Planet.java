4c05125ebc09f65593af8a4b46a1ec721b3fe275
MiniatureWheats <a.c.kellogg@wustl.edu>
2026-04-21 14:10:47 -0500

    q2 fixed

--- src/exam/codewriting/q2/Planet.java
+++ src/exam/codewriting/q2/Planet.java
@@ -9,33 +9,33 @@ public class Planet {
 
 	// Define your instance variables here
 	private String name;
-	private double mass;
+	private int mass;
+
 	// Write the constructor for the Planet class
-	public Planet(String name, double mass){
-		this.name=name;
-		this.mass=mass;
+	public Planet(String name, int mass) {
+		this.name = name;
+		this.mass = mass;
 	}
 
-	public String getName () {
-		return name; //FIXME
+	public String getName() {
+		return name; // FIXME
 	}
-	
-	public int getMass () {
-		return mass; //FIXME
+
+	public int getMass() {
+		return mass; // FIXME
 	}
-	
-	public String toString () {
-		return name + ", " + mass; 
-	} 
-	
+
+	public String toString() {
+		return name + ", " + mass;
+	}
+
 	/**
-	* When two planets collide, a new Planet should be created and returned.
-	* The mass of the new planet should be the total mass of the two planets
-	* involved in the collision, and it should be named after both planets.
-	*/
-	public Planet collide ( Planet other ) {
-		return new Planet(name + "-" + other.name, mass + other.mass); //FIXME
+	 * When two planets collide, a new Planet should be created and returned.
+	 * The mass of the new planet should be the total mass of the two planets
+	 * involved in the collision, and it should be named after both planets.
+	 */
+	public Planet collide(Planet other) {
+		return new Planet(name + "-" + other.name, mass + other.mass); // FIXME
 	}
-	
-	
+
 }
--- src/exam/codewriting/q2/SolarSystem.java
+++ src/exam/codewriting/q2/SolarSystem.java
@@ -10,54 +10,56 @@ provided to you below. Examine the instructions and complete the remaining metho
 public class SolarSystem {
 
 	// Instance variables (do not change)
-	private String name ;
-	private LinkedList < Planet > planets ;
+	private String name;
+	private LinkedList<Planet> planets;
 	// Write the constructor for the SolarSystem class.
 
-	public SolarSystem(String name){
-		this.name=name;
-		LinkedList planets = new LinkedList<>();
+	public SolarSystem(String name) {
+		this.name = name;
+		this.planets = new LinkedList<>();
 	}
 	// When a SolarSystem is first created, it should not contain any planets.
-	
-	public String getName () {
-		return name; //FIXME
+
+	public String getName() {
+		return name; // FIXME
 	}
-	
-	public void addPlanet ( Planet p) {
+
+	public void addPlanet(Planet p) {
 		planets.add(p);
 	}
-	
-	public int getNumberOfPlanets () {
-		return planets.size(); //FIXME
+
+	public int getNumberOfPlanets() {
+		return planets.size(); // FIXME
 	}
-	
+
 	/**
-	* Returns the sum of the masses of all planets in the solar system
-	*/
-	public double getTotalMass () {
-		double totalMass =0;
-		for(Planet p: planets){
+	 * Returns the sum of the masses of all planets in the solar system
+	 */
+	public int getTotalMass() {
+		int totalMass = 0;
+		for (Planet p : planets) {
 			totalMass += p.getMass();
 		}
-		return totalMass; //FIXME
+		return totalMass; // FIXME
 	}
-	
-	public static void main(String[] args) {
-		//Write a line of code that creates an instance of the Planet class by calling the constructor. You should
-		//also declare a variable to hold the newly created instance.
-		Planet jupiter = new Planet("Jupiter", 256783);
-		Planet saturn = new Planet("Saturn", 174592.5);
 
-		//Write a line of code that creates an instance of the SolarSystem class by calling the constructor. You
-		//should also declare a variable to hold the newly created instance.
+	public static void main(String[] args) {
+		// Write a line of code that creates an instance of the Planet class by calling
+		// the constructor. You should
+		// also declare a variable to hold the newly created instance.
+		Planet jupiter = new Planet("Jupiter", 256783);
+		Planet saturn = new Planet("Saturn", 174592);
+
+		// Write a line of code that creates an instance of the SolarSystem class by
+		// calling the constructor. You
+		// should also declare a variable to hold the newly created instance.
 		SolarSystem sun = new SolarSystem("Sun");
 
-		//Write a line of code that will add the Planet to the SolarSystem
+		// Write a line of code that will add the Planet to the SolarSystem
 		sun.addPlanet(jupiter);
 		sun.addPlanet(saturn);
-		
-		//Write a line of code that will retrieve the total mass from the SolarSystem
-		sun.getTotalMass();
+
+		// Write a line of code that will retrieve the total mass from the SolarSystem
+		System.out.println(sun.getTotalMass());
 	}
 }
